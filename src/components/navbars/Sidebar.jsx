4b1d6b71f36f80ea2e86a357149a693377e6d9b4
Dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-04-26 07:34:04 -0600

    Actualizacion de src

--- src/components/navbars/Sidebar.jsx
+++ src/components/navbars/Sidebar.jsx
@@ -1,28 +1,29 @@
-export const sidebar = ({ channels }) => {
-    if (!channels) {
+/* eslint-disable react/prop-types */
+
+
+export const Sidebar = ({channels}) =>{
+    if(!channels){
         return null
     }
- 
-    return (
-      <div className="sidebar-container">
-        <span className="sidebar-title">sugeridos</span>
-            <span className="sidebar-subtitle">Canales que sigo</span>
+
+    return(
+        <div className="sidebar-container">
+            <span className="sidebar-title">Sugeridos</span>
+            <span className="sidebar-subtitle">CANALES QUE SIGO</span>
             {channels.map((channel) => {
-                return (
-                  <div key={channel.id} className="sidebar-list-item">
+                return(
+                    <div key={channel.id} className="sidebar-list-item">
                         <span className="sidebar-list-username">{channel.username}</span>
                         <span className="sidebar-list-status"
                             style={{
                                 color: channel.isOnline ? 'green' : 'red'
                             }}
                         >
-                            { channel.isOnline ? 'Online' : 'Offline' }
+                         {channel.isOnline ? 'Online' : 'Ofline'}
                         </span>
-                  </div>
-                );
-            })
-               
-            }
-      </div>
-    );
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,9 +1,25 @@
 import axios from "axios";
+import { logout } from "../shared/hooks";
 
 const apiClient = axios.create({
     baseURL: 'http://127.0.0.1:3001/twitch/v1',
-    timeout: 1000
+    timeout: 5000
 })
+
+apiClient.interceptors.request.use(
+    (config) => {
+        const userDetails = localStorage.getItem('user')
+    
+        if(userDetails){
+            const token = JSON.parse(userDetails).token
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (e) => {
+        return Promise.reject(e)
+    }
+)
 
 export const login = async (data) => {
     try{
@@ -11,7 +27,7 @@ export const login = async (data) => {
     }catch(e){
         return{
             error: true,
-            e
+            e 
         }
     }
 }
@@ -38,10 +54,33 @@ export const getChannels = async () => {
     }
 }
 
+export const getChannelSettings = async () => {
+    try{
+        return await apiClient.get('/settings/channel')
+    }catch(e){
+        return{
+            error: true,
+            e
+        }
+    }
+}
+
+export const updateChannelSettings = async (data) => {
+    try{
+        return await apiClient.put('/settings/channel', data)
+    }catch(e){
+        return{
+            error: true,
+            e
+        }
+    }
+}
+
 export const getFollowedChannels = async () => {
     try{
-        return await apiClient.get('/channels')
+        return await apiClient.get('/channels/followed')
     }catch(e){
+        checkResponseStatus(e)
         return{
             error: true,
             e: e
@@ -53,6 +92,6 @@ const checkResponseStatus = (e) => {
     const responseStatus = e?.response?.status
 
     if(responseStatus){
-        (responseStatus === 401 || responseStatus === 403) && logout()
+        (responseStatus === 401 || responseStatus === 403) && logout
     }
 }
